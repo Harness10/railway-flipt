b7f97f6f8d08fc212b30782dd040cf09955031e0
vehm <github@vehm.dev>
2026-03-26 11:34:07 -0500

    removed logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 set -euo pipefail
 sudo chown -R flipt:flipt /var/opt/flipt
-
-# Railway Debugging
-echo "HOME=$HOME"
-echo "XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-not set}"
-ls -la $HOME/.config/flipt/ 2>&1 || echo "config dir not found"
-
 exec /flipt server
